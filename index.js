860373aec1c2079e04463450739a66a75be9003a
EthanP94 <99214691+EthanP94@users.noreply.github.com>
2022-03-24 16:55:28 -0700

    created class, and quetions to be asked from the prompt. FInished bulk of coding. It generated HTML based off the questions answered and number of employees

---
+++ index.js
@@ -0,0 +1,139 @@
+const inquirer = require("inquirer")
+const path = require("path")
+const fs = require("fs")
+
+const engineer = require("./lib/Engineer")
+const manager = require("./lib/Manager")
+const intern = require("./lib/Inter")
+
+const {
+    generateEngineerCard,
+    generateInternCard,
+    generateManagerCard,
+    baseHtml} 
+    = require("./src/htmlGen")
+
+const teamMemberHtmlArr = [];
+
+function init() {
+    function managerCreate() {
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is the Managers name?',
+              },
+              {
+                type: 'input',
+                name: 'email',
+                message: 'Whats your email?',
+              },
+              {
+                type: 'input',
+                name: 'id',
+                message: 'What is your id number?',
+              },
+              {
+                type: 'input',
+                name: 'office',
+                message: 'What is your office phone number?',
+              },
+        ])
+        .then(({name, id, email, officeNumber}) => {
+                const manager = new Manager(id, name, email, officeNumber);
+                teamMemberHtmlArr.push(generateManagerCard(manager));
+                mainMenu();
+        });
+        
+    }
+    function mainMenu() {
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'addedRole',
+                message: 'Would you like to add another Employee?',
+                choices: ['Engineer', 'Intern', "No, I'm Done"],
+            },
+        ])
+        .then(answers => {
+            switch (answers.addedRole) {
+                case "Engineer":
+                    return engineerCreate();
+                case "Intern":
+                    return internCreate()
+                default: 
+                    return generateHtml;
+            }
+        })
+    }
+    function engineerCreate() {
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is the Engineers name?',
+              },
+              {
+                type: 'input',
+                name: 'email',
+                message: 'Whats your email?',
+              },
+              {
+                type: 'input',
+                name: 'id',
+                message: 'What is the Engineer Id number?',
+              },
+              {
+                type: 'input',
+                name: 'iithub',
+                message: 'What is your Github?',
+              },
+        ])
+        .then(({name, id, email, github}) => {
+                const engineer = new engineer(id, name, email, github);
+                teamMemberHtmlArr.push(generateEngineerCard(engineer));
+                mainMenu();
+        });
+
+    }
+    function internCreate() {
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is the Interns name?',
+              },
+              {
+                type: 'input',
+                name: 'email',
+                message: 'Whats your email?',
+              },
+              {
+                type: 'input',
+                name: 'id',
+                message: 'What is the Interns Id number?',
+              },
+              {
+                type: 'input',
+                name: 'iithub',
+                message: 'What is your school?',
+              },
+        ])
+        .then(({name, id, email, school}) => {
+                const intern = new Intern(id, name, email, school);
+                teamMemberHtmlArr.push(generateInternCard(intern));
+                mainMenu();
+        });
+
+    }
+
+    function generateHtml (){
+        fs.watchFile("./dist/index.html", baseHtml(teamMemberHtmlArr), (err) => {
+            err ? console.log(err) : console.log("Generated HTML File")
+        })
+    };
+
+    managerCreate();
+
+}
+init();
---
+++ src/htmlGen.js
@@ -0,0 +1,80 @@
+const generateManagerCard = (manager) => {
+    return `
+    <div class="card">
+        <h3>${manager.getname()}<h3>
+        <p><i class="fa-solid fa-mug-hot"></i>${manager.getRole()}r</p>
+        <div class="card-body">
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">${manager.getId()}</li>
+                <li class="list-group-item">Email: <a herf="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+                <li class="list-group-item">${manager.getOfficeNumber()}</li>
+            </ul>
+        </div>
+    </div>
+    `
+}
+
+const generateEngineerCard = (engineer) => {
+    return `
+    <div class="card">
+        <h3>${engineer.getname()}<h3>
+        <p><i class="fa-solid fa-mug-hot"></i>${engineer.getRole()}r</p>
+        <div class="card-body">
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">${engineer.getId()}</li>
+                <li class="list-group-item">Email: <a herf="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+                <li class="list-group-item">${engineer.getGithub()}</li>
+            </ul>
+        </div>
+    </div>
+    `
+}
+
+const generateInternCard = (intern) => {
+    return `
+    <div class="card">
+        <h3>${intern.getname()}<h3>
+        <p><i class="fa-solid fa-mug-hot"></i>${intern.getRole()}r</p>
+        <div class="card-body">
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">${intern.getId()}</li>
+                <li class="list-group-item">Email: <a herf="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+                <li class="list-group-item">${intern.getSchool()}</li>
+            </ul>
+        </div>
+    </div>
+    `
+}
+
+const baseHtml = (hmltArr) => `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="jumbotron jumbotron-fluid">
+            <h1 class="display-4 heading">My Team</h1>
+        </div>
+    </header>
+    <div id="container">
+    <div class="card-deck">
+        ${htmlArr.join(' ')}
+
+    </div>
+    </div>
+
+</body>
+</html>
+`
+
+module.exports = {
+    generateEngineerCard,
+    generateInternCard,
+    generateManagerCard,
+    baseHtml
+}
