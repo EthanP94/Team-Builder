7b4db791e0554c0b027349f87bb8fe521ca81270
EthanP94 <99214691+EthanP94@users.noreply.github.com>
2022-03-25 19:24:50 -0700

    added more styling for cards and homepage

--- src/htmlGen.js
+++ src/htmlGen.js
@@ -1,8 +1,8 @@
 const generateManagerCard = (manager) => {
     return `
-    <div class="bg-primary mb-3 col-4 mt-4" style="max-width: 18rem;">
+    <div class="col-4 mt-4">
         <div class="card h-100">
-            <div class="card-header">
+            <div class="card-header bg-primary">
                 <h3>${manager.getName()}</h3>
                 <h4>Manager</h4><i class="material-icons">content_paste</i>
             </div>
@@ -18,9 +18,9 @@ const generateManagerCard = (manager) => {
 
 const generateEngineerCard = (engineer) => {
     return `
-    <div class="bg-success mb-3 col-4 mt-4" style="max-width: 18rem;">
+    <div class="col-4 mt-4">
         <div class="card h-100">
-            <div class="card-header">
+            <div class="card-header bg-success">
                 <h3>${engineer.getName()}</h3>
                 <h4>Engineer</h4><i class="material-icons">laptop_mac</i>
             </div>
@@ -36,9 +36,9 @@ const generateEngineerCard = (engineer) => {
 
 const generateInternCard = (intern) => {
     return `
-    <div class="bg-info mb-3 col-4 mt-4" style="max-width: 18rem;>
+    <div class="col-4 mt-4">
         <div class="card h-100">
-            <div class="card-header">
+            <div class="card-header bg-info">
                 <h3>${intern.getName()}</h3>
                 <h4>Intern</h4><i class="material-icons">assignment_ind</i>
             </div>
@@ -68,19 +68,19 @@ const baseHtml = (htmlArr) => `
 <main>
     <header>
         <div class="jumbotron jumbotron-fluid">
-            <h1 class="display-4 heading">My Team</h1>
+            <h1 class="display-4 heading text-center">My Team</h1>
         </div>
     </header>
     <div id="container">
-    <div class="card-deck">
+    <div class="row justify-content-center" id="card-deck">
         ${htmlArr.join(' ')}
     </div>
     </div>
   </main>
 </body>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 </html>
 `
 
