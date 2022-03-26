da2b8686e4c57c57cd920879d08da370f566971e
EthanP94 <99214691+EthanP94@users.noreply.github.com>
2022-03-25 17:09:49 -0700

    fixed function to generate html

--- src/htmlGen.js
+++ src/htmlGen.js
@@ -46,14 +46,14 @@ const generateInternCard = (intern) => {
     `
 }
 
-const baseHtml = (hmltArr) => `
+const baseHtml = (htmlArr) => `
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta N="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
     <title>Document</title>
 </head>
 <body>
