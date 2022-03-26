fbdb609fdea535e7ffe08a9c15e7fa38e5ab60e6
EthanP94 <99214691+EthanP94@users.noreply.github.com>
2022-03-25 18:48:34 -0700

    adding styling to individual cards

--- src/htmlGen.js
+++ src/htmlGen.js
@@ -1,14 +1,16 @@
 const generateManagerCard = (manager) => {
     return `
-    <div class="card">
-        <h3>${manager.getName()}<h3>
-        <p><i class="fa-solid fa-mug-hot"></i>${manager.getRole()}r</p>
-        <div class="card-body">
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">${manager.getId()}</li>
-                <li class="list-group-item">Email: <a herf="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
-                <li class="list-group-item">${manager.getOfficeNumber()}</li>
-            </ul>
+    <div class="bg-primary mb-3 col-4 mt-4" style="max-width: 18rem;">
+        <div class="card h-100">
+            <div class="card-header">
+                <h3>${manager.getName()}</h3>
+                <h4>Manager</h4><i class="material-icons">content_paste</i>
+            </div>
+            <div class="card-body">
+                <p class="id">ID: <h3>${manager.getId()}</p>
+                <p class="email">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></p>
+                <p class="office">Office Number: ${manager.getOfficeNumber()}</p>
+            </div>
         </div>
     </div>
     `
@@ -16,15 +18,17 @@ const generateManagerCard = (manager) => {
 
 const generateEngineerCard = (engineer) => {
     return `
-    <div class="card">
-        <h3>${engineer.getName()}<h3>
-        <p><i class="fa-solid fa-mug-hot"></i>${engineer.getRole()}r</p>
-        <div class="card-body">
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">${engineer.getId()}</li>
-                <li class="list-group-item">Email: <a herf="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
-                <li class="list-group-item">${engineer.getGithub()}</li>
-            </ul>
+    <div class="bg-success mb-3 col-4 mt-4" style="max-width: 18rem;">
+        <div class="card h-100">
+            <div class="card-header">
+                <h3>${engineer.getName()}</h3>
+                <h4>Engineer</h4><i class="material-icons">laptop_mac</i>
+            </div>
+            <div class="card-body">
+                <p class="id">ID: <h3>${engineer.getId()}</p>
+                <p class="email">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></p>
+                <p class="github">Github: <a href="https://github.com/${engineer.getGithub()}">${engineer.getGithub()}</a></p>
+            </div> 
         </div>
     </div>
     `
@@ -32,15 +36,17 @@ const generateEngineerCard = (engineer) => {
 
 const generateInternCard = (intern) => {
     return `
-    <div class="card">
-        <h3>${intern.getName()}<h3>
-        <p><i class="fa-solid fa-mug-hot"></i>${intern.getRole()}r</p>
-        <div class="card-body">
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">${intern.getId()}</li>
-                <li class="list-group-item">Email: <a herf="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
-                <li class="list-group-item">${intern.getSchool()}</li>
-            </ul>
+    <div class="bg-light mb-3 col-4 mt-4" style="max-width: 18rem;>
+        <div class="card h-100">
+            <div class="card-header">
+                <h3>${intern.getName()}</h3>
+                <h4>Intern</h4><i class="material-icons">assignment_ind</i>
+            </div>
+            <div class="card-body">
+                <p class="id">ID: <h3>${intern.getId()}</p>
+                <p class="email">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></p>
+                <p class="school">School: ${intern.getSchool()}</p>
+            </div>
         </div>
     </div>
     `
@@ -53,11 +59,13 @@ const baseHtml = (htmlArr) => `
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta N="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <title>Document</title>
 </head>
-<body>
+<main>
     <header>
         <div class="jumbotron jumbotron-fluid">
             <h1 class="display-4 heading">My Team</h1>
@@ -68,9 +76,11 @@ const baseHtml = (htmlArr) => `
         ${htmlArr.join(' ')}
     </div>
     </div>
-  </body>
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+  </main>
 </body>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 </html>
 `
 
